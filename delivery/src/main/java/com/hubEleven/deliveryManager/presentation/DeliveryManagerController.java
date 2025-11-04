784f3adbf59c23ba8393e9eee30ac2f0a43198bb
ming <oterges00@gmail.com>
2025-11-04 20:05:44 +0900

    [config] delivery 서비스 MSA 기본 설정

---
+++ delivery/src/main/java/com/hubEleven/deliveryManager/presentation/DeliveryManagerController.java
@@ -0,0 +1,10 @@
+package com.hubEleven.deliveryManager.presentation;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeliveryManagerController {
+
+
+}
