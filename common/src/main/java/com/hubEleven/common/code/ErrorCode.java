ad6191c0f0cb51cb57e80be4366bbc8ae26db481
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-11-05 22:32:21 +0900

    [feat] 성공/에러 코드 추가

--- common/src/main/java/com/hubEleven/common/code/ErrorCode.java
+++ common/src/main/java/com/hubEleven/common/code/ErrorCode.java
@@ -9,7 +9,13 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum ErrorCode implements StatusCode {
-	SERVER_ERROR(INTERNAL_SERVER_ERROR, "서버 에러가 발생하였습니다.");
+	SERVER_ERROR(INTERNAL_SERVER_ERROR, "서버 에러가 발생하였습니다."),
+	VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "요청 값이 유효하지 않습니다."),
+	ACCESS_DENIED(HttpStatus.FORBIDDEN, "권한이 없습니다."),
+
+	// Company
+	COMPANY_DUPLICATED(HttpStatus.CONFLICT, "같은 허브에 동일한 업체명이 이미 존재합니다."),
+	COMPANY_NOT_FOUND(HttpStatus.NOT_FOUND, "업체를 찾을 수 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
