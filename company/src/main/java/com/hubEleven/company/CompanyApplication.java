bd88165a064270029db1e4188d6b19cdaa2a07bb
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-11-06 17:22:31 +0900

    [feat] @EnableDiscoveryClient 추가

--- company/src/main/java/com/hubEleven/company/CompanyApplication.java
+++ company/src/main/java/com/hubEleven/company/CompanyApplication.java
@@ -2,8 +2,10 @@ package com.hubEleven.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.hubEleven.company.infrastructure.client")
 @SpringBootApplication
 public class CompanyApplication {
