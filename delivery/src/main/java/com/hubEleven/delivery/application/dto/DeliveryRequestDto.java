9c143755c47aa2b94b1c8439ec0e7c1c370d66c6
juyoung <juyoung412@gmail.com>
2025-11-07 14:50:36 +0900

    sportless 재 적용

--- delivery/src/main/java/com/hubEleven/delivery/application/dto/DeliveryRequestDto.java
+++ delivery/src/main/java/com/hubEleven/delivery/application/dto/DeliveryRequestDto.java
@@ -4,12 +4,10 @@ import com.hubEleven.delivery.domain.DeliveryStatus;
 import java.util.UUID;
 
 public record DeliveryRequestDto(
-        UUID orderId,
-        DeliveryStatus status,
-        UUID fromHubId,
-        UUID toHubId,
-        String recipientName,
-        String recipientSlackId,
-        Long deliveryManagerId) {
-
-}
+		UUID orderId,
+		DeliveryStatus status,
+		UUID fromHubId,
+		UUID toHubId,
+		String recipientName,
+		String recipientSlackId,
+		Long deliveryManagerId) {}
