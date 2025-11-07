a59bb67bf4205ec3b32695b78216fd5b69b1ef09
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-11-07 14:58:00 +0900

    [feat] AI 엔티티에 id 추가

--- notification/src/main/java/com/hubEleven/notification/domain/model/AiRequestLog.java
+++ notification/src/main/java/com/hubEleven/notification/domain/model/AiRequestLog.java
@@ -3,13 +3,21 @@ package com.hubEleven.notification.domain.model;
 import com.hubEleven.common.annotation.SoftDeletable;
 import com.hubEleven.common.model.BaseEntity;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 import java.util.UUID;
 
+
 @Entity
+@Getter
 @Table(name = "p_ai_request_log")
 @SoftDeletable
 public class AiRequestLog extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "ai_request_log_id", nullable = false, updatable = false)
+    private UUID id;
 
     @Column(name = "order_id", nullable = false)
     private UUID orderId;
@@ -31,4 +39,19 @@ public class AiRequestLog extends BaseEntity {
         this.status = status;
     }
 
+    public static AiRequestLog requested(UUID orderId) {
+        return new AiRequestLog(orderId, RequestStatus.REQUESTED);
+    }
+
+    public void success(String prompt, String response) {
+        this.status = RequestStatus.SUCCESS;
+        this.rawPrompt = prompt;
+        this.rawResponse = response;
+    }
+
+    public void fail(String prompt, String response) {
+        this.status = RequestStatus.FAIL;
+        this.rawPrompt = prompt;
+        this.rawResponse = response;
+    }
 }
