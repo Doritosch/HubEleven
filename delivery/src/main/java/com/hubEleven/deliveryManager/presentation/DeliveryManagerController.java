4d62b6bde8e4f32c99471fed03080f37a18b5428
ming <oterges00@gmail.com>
2025-11-10 19:10:25 +0900

    [docs] DeliveryManager swagger 적용

--- delivery/src/main/java/com/hubEleven/deliveryManager/presentation/DeliveryManagerController.java
+++ delivery/src/main/java/com/hubEleven/deliveryManager/presentation/DeliveryManagerController.java
@@ -10,6 +10,8 @@ import com.hubEleven.deliveryManager.presentation.dto.request.DeliveryManagerAss
 import com.hubEleven.deliveryManager.presentation.dto.request.DeliveryManagerCreateRequestDto;
 import com.hubEleven.deliveryManager.presentation.dto.response.DeliveryManagerAssignResponseDto;
 import com.hubEleven.deliveryManager.presentation.dto.response.DeliveryManagerResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
@@ -26,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "DeliveryManager", description = "배송담당자 API")
 @Slf4j
 @RestController
 @RequestMapping("/v1/delivery-managers")
@@ -37,6 +40,7 @@ public class DeliveryManagerController {
 		this.deliveryManagerService = deliveryManagerService;
 	}
 
+    @Operation(summary = "배송 담당자 생성 API", description = "새로운 배송담당자를 생성한다.")
 	@PostMapping
 	public ResponseEntity<ApiResponse<DeliveryManagerResponseDto>> createDeliveryManager(
 			@RequestBody @Valid DeliveryManagerCreateRequestDto createRequestDto) {
@@ -48,7 +52,8 @@ public class DeliveryManagerController {
         return ApiResponseEntity.success(responseDto);
 	}
 
-	@GetMapping
+    @Operation(summary = "배송 담당자 목록 조회 API", description = "전체 배송담당자 목록을 조회한다.")
+    @GetMapping
 	public ResponseEntity<ApiResponse<CommonPageResponse<DeliveryManagerResponseDto>>>
     getAllDeliveryManager(
         @RequestParam(defaultValue = "0") int page,
@@ -68,7 +73,8 @@ public class DeliveryManagerController {
         return ApiResponseEntity.success(responseDtoList);
 	}
 
-	@GetMapping("/{managerId}")
+    @Operation(summary = "배송 담당자 단일 조회 API", description = "특정 배송담당자를 조회한다.")
+    @GetMapping("/{managerId}")
     public ResponseEntity<ApiResponse<DeliveryManagerResponseDto>> getDeliveryManager(
 			@PathVariable Long managerId) {
 		log.info("[DeliveryManager Controller] 배달 담당자 단일 조회 요청");
@@ -76,13 +82,15 @@ public class DeliveryManagerController {
         return ApiResponseEntity.success(responseDto);
 	}
 
-	@DeleteMapping("/{managerId}")
+    @Operation(summary = "배송 담당자 삭제 API", description = "배송 담당자를 삭제한다.")
+    @DeleteMapping("/{managerId}")
     public ResponseEntity<ApiResponse<Void>> deleteDeliveryManager(@PathVariable Long managerId) {
 		deliveryManagerService.deleteDeliveryManager(managerId);
         return ApiResponseEntity.ok("삭제가 완료되었습니다.");
 	}
 
-	@PatchMapping("/assign")
+    @Operation(summary = "배송 담당자 배정 API", description = "특정 배송경로에 대한 배송 담당자를 배정한다.")
+    @PatchMapping("/assign")
 	public ResponseEntity<DeliveryManagerAssignResponseDto> assignDeliveryManager(
 			@RequestBody @Valid DeliveryManagerAssignRequestDto assignRequestDto) {
 		DeliveryManagerAssignResponseDto assignResponseDtoList =
@@ -93,6 +101,7 @@ public class DeliveryManagerController {
 
 
     //MSA 서버 내부용 메서드
+    @Operation(summary = "배송 담당자 존재 여부 확인 API", description = "특정 베송 담당자가 존재하는지 확인한다.")
     @GetMapping("/exist")
     public boolean checkDeliveryManagerExists(Long managerId) {
         return deliveryManagerService.checkDeliveryManagerExists(managerId);
@@ -101,6 +110,7 @@ public class DeliveryManagerController {
 
     //MSA 서버 내부용 메서드
     //업체배송담당 매니저 소속확인 - 해당허브에 해당매니저가 존재하는지 검증
+    @Operation(summary = "업체배송 담당자 소속 확인 API", description = "베송 담당자가 특정 허브에 소속되었는지 확인한다.")
     @GetMapping("/exist/hub")
     public boolean hasDeliveryManagerInHub(UUID hubId, Long managerId) {
         return deliveryManagerService.hasDeliveryManagerInHub(hubId, managerId);
