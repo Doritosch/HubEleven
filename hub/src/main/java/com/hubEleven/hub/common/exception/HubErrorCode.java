b1f524246e2fd3ed106992b88fa158cb3c468715
Hyunho Kang <hhwj2270@gmail.com>
2025-11-10 15:03:51 +0900

    [chore] 경로 에러코드 추가

--- hub/src/main/java/com/hubEleven/hub/common/exception/HubErrorCode.java
+++ hub/src/main/java/com/hubEleven/hub/common/exception/HubErrorCode.java
@@ -11,7 +11,8 @@ public enum HubErrorCode implements StatusCode {
 	DUPLICATE_HUB_NAME(HttpStatus.BAD_REQUEST, "이미 동일한 이름의 허브가 존재합니다."),
 	HUB_NOT_FOUND(HttpStatus.NOT_FOUND, "허브를 찾을 수 없습니다."),
 	GEOCODING_FAILED(HttpStatus.BAD_REQUEST, "주소로부터 좌표를 찾을 수 없습니다."),
-	KAKAO_API_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "카카오 API 호출 중 오류가 발생했습니다.");
+	KAKAO_API_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "카카오 API 호출 중 오류가 발생했습니다."),
+	ROUTE_NOT_FOUND(HttpStatus.NOT_FOUND, "경로를 찾을 수 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
