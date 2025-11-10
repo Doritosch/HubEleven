fbb575807db761efae315da338b41fcdb54d118e
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-11-10 16:32:26 +0900

    [feat] WebClient 기반 Slack webhook 호출 클라이언트 추가

---
+++ notification/src/main/java/com/hubEleven/notification/slack/application/dto/SlackWebhookResponse.java
@@ -0,0 +1,8 @@
+package com.hubEleven.notification.slack.application.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record SlackWebhookResponse(
+        @JsonProperty("ok") Boolean ok,
+        @JsonProperty("error") String error
+) {}
