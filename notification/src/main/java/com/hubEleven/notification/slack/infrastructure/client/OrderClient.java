d56164e6b51869a9e17d4203112038850b769aaf
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-11-11 10:52:02 +0900

    [refactor] 이름 변경

--- notification/src/main/java/com/hubEleven/notification/slack/infrastructure/client/OrderClient.java
+++ notification/src/main/java/com/hubEleven/notification/slack/infrastructure/client/OrderClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "order-service")
-public interface OrderServiceClient {
+public interface OrderClient {
 	@GetMapping("/v1/orders/{orderId}")
 	OrderDTO getOrder(@PathVariable UUID orderId);
 
