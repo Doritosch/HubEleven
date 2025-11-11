2e5ddafc35c4b47ab9240b9051c95441b3ab12d0
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-11-11 20:32:58 +0900

    [feat] GlobalExceptionHandlerTest 구현

---
+++ user/src/main/java/com/hubEleven/user/domain/exception/GlobalExceptionHandlerTest.java
@@ -0,0 +1,8 @@
+package com.hubEleven.user.domain.exception;
+
+import com.commonLib.common.exception.GlobalExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GlobalExceptionHandlerTest extends GlobalExceptionHandler {
+}
