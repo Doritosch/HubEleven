cb71d51501d1b5fa4bc48fc36989fbaedc3ca170
S2hyeyunS2 <hhyeyun22@gmail.com>
2026-01-05 15:34:29 +0900

    [refactor] 패키지명 수정

--- company/src/main/java/com/hubEleven/company/infrastructure/config/OpenApiConfig.java
+++ company/src/main/java/com/hubEleven/company/infrastructure/config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package com.hubEleven.company.infrastructure.configuration;
+package com.hubEleven.company.infrastructure.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
